dbc2e753b6346e56be7e68f1b14b979ac609e055
belencera <belen_cera@hotmail.com>
2026-07-14 12:39:51 +0200

    Cambio de subtítulo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
             <img src={logo} alt="Interview Tracker Logo" className="logo" />
             <div className="header-content">
                 <h1>Interview Tracker</h1>
-                <h2>Registra el progreso de tus entrevistas de trabajo</h2>
+                <h2>Haz el seguimiento de todas tus candidaturas de forma sencilla</h2>
             </div>
         </header>
     )
